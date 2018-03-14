159193a7b488ebc647dff01bb48beea78ef818e4
Kukaratza <marioandres.martinez80@gmail.com>
2018-03-13 20:09:44 -0700

    working on form input to firebase

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,40 +10,46 @@ var config = {
 firebase.initializeApp(config);
 
 let database = firebase.database();
+let playersArray = [];
 let wins = 0;
 let loses = 0;
-let choice = true;
+let choice = "";
 let turn = 1;
 
 $("#submit").on("click", function () {
     event.preventDefault();
-    let player = $("#inputUser").val();
-    let player2 = $("#inputUser2").val();
-    console.log(player);
+    for (let i = 0; i < 2; i++) {
+        playersArray.push($("#inputUser").val());
+        console.log(playersArray);
+    }
 
-    database.ref().push({
-        turn : turn, 
-        players : {
-            player1 : {
-                name: player,
-                choice: choice,
-                wins: wins,
-                loses: loses,
-            },
-            player2 : {
-                name: player,
-                choice: choice,
-                wins: wins,
-                loses: loses,
-            },
-        },
-    });
+    // database.ref().push({
+    //     turn: turn,
+    //     players: {
+    //         1: {
+    //             name: playersArray[0],
+    //             choice: choice,
+    //             wins: wins,
+    //             loses: loses,
+    //         },
+    //         2: {
+    //             name: playersArray[1],
+    //             choice: choice,
+    //             wins: wins,
+    //             loses: loses,
+    //         },
+    //     },
+    // });
 });
 
-database.ref().on("child_added", function(snapshot) {
+// console.log(Player1);
+// console.log(Player2);
+
+
+
+database.ref().on("child_added", function (snapshot) {
     console.log(snapshot.val());
     console.log(snapshot.val().player);
-  }, function(errorObject) {
+}, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
-  });
-
+});
